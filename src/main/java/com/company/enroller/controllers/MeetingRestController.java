1c02124bae904f87e91ee628e2821ce53660d62f
BM <mbal@wp.pl>
2022-04-13 12:08:58 +0200

    checkpoint 3 completed - add meetings

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -2,6 +2,7 @@ package com.company.enroller.controllers;
 
 import java.util.Collection;
 
+import com.company.enroller.model.Participant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,26 @@ public class MeetingRestController {
     public ResponseEntity<?> getMeetings() {
         Collection<Meeting> meetings = meetingService.getAll();
         return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeeting(@PathVariable("id") long id) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<?> registerMeeting(@RequestBody Meeting meeting) {
+        Meeting foundMeeting = meetingService.findById(meeting.getId());
+        if  (foundMeeting != null) {
+            return new ResponseEntity<String>("Unable to create. Meeting already exists", HttpStatus.CONFLICT);
+        }
+
+        meetingService.add(meeting);
+        return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
     }
 
 }
